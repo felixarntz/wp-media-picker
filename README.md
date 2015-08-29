59e534c44e2bc130624c760ac57ba81bd1adbd2d
Felix Arntz <felix-arntz@t-online.de>
2015-08-30 00:49:26 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# WP Media Picker
+
+This jQuery plugin for WordPress can be used to transform an input field into a flexible and compatible media field with attachment selection and preview.
+
+![Plugin Screenshot](https://raw.githubusercontent.com/felixarntz/wp-media-picker/master/screenshot.png)
+
+## Features
+
+* transforms a simple input field into a media picker with buttons to open the WordPress media modal
+* handles the custom media modal behavior as well as how the value is stored in the input field (you're free to choose between ID and URL)
+* renders a preview of the attachment if it is an image, video or audio file
+* uses WordPress Core technology wherever possible
+* fully compatible with WordPress Core media modal, so it can be used on post editing screens too
+* customizable with numerous settings which can be defined in the function call or as data attributes
+* since it only uses WordPress Core strings for the labels, it has probably been translated into your language already
+
+## Installation and Setup
+
+### Install the plugin
+
+The preferred method to install this package is to use Bower.
+`bower install felixarntz/wp-media-picker`
+
+### Enqueue script and stylesheet
+
+To include the script and stylesheet, enqueue the script and stylesheet like so:
+```php
+<?php
+
+wp_enqueue_media();
+wp_enqueue_script( 'wp-media-picker', 'PATHTOMEDIAPICKER/wp-media-picker.min.js', array( 'jQuery', 'media-editor' ), '0.1.0', true );
+wp_enqueue_style( 'wp-media-picker', 'PATHTOMEDIAPICKER/wp-media-picker.min.css', array(), '0.1.0' );
+
+```
+
+Make sure to use the proper hook to enqueue the assets, for example in the `admin_enqueue_scripts` hook. Furthermore the dependencies in the above code sample must explicitly be included, otherwise the plugin will not work.
+
+### Initialize the plugin on your fields
+
+To turn your raw and boring input fields into really exciting media picker fields, you simply need to run the main plugin function `wpMediaPicker()` on your jQuery elements. For example:
+
+```js
+jQuery( '.custom-media-field' ).wpMediaPicker();
+```
+
+## Plugin Settings
+
+The plugin supports numerous settings so that you can tweak how your fields work. There are two ways to apply settings to a field: Either specify the settings (as an object) when initializing the plugin in Javascript, or put the settings into a `data-settings` attribute on the field (in JSON format).
+
+Here you find a list of all available settings:
+
+Setting                  | Description                                                                                                             | Default Value
+------------------------ | ----------------------------------------------------------------------------------------------------------------------- | -------------
+`store`                  | Determines how the attachment is stored in the input field. Accepts 'id' or 'url'                                       | 'url'
+`query`                  | Alters the attachments query in the media library (for example to only show images, use `{ post_mime_type: 'image' }`)  | empty object
+`filterable`             | Whether the library is filterable, and if so what filters should be shown. Accepts 'all', 'uploaded' or 'unattached'    | 'all'
+`searchable`             | Whether the library is searchable. Accepts a boolean                                                                    | true
+`editable`               | Whether the library content is editable. Accepts a boolean                                                              | false
+`allowLocalEdits`        | Whether the library content can be edited locally (only used if `editable` is false). Accepts a boolean                 | false
+`displaySettings`        | Whether to show the attachment display settings. Accepts a boolean                                                      | false
+`displayUserSettings`    | Whether to update the user settings when editing attachment display settings. Accepts a boolean                         | false
+`label_add`              | Sets the text for the add button on the field. Accepts a string                                                         | 'Add Media'
+`label_replace`          | Sets the text for the replace button on the field. Accepts a string                                                     | 'Replace'
+`label_remove`           | Sets the text for the remove button on the field. Accepts a string                                                      | 'Remove'
+`label_modal`            | Sets the title text for the media modal. Accepts a string                                                               | 'Add Media'
+`label_button`           | Sets the button text for the media modal. Accepts a string                                                              | 'Add Media'
+
+## Contribute
+
+I'm always grateful for contributions, whether it is about enhancements or bugfixes, especially since the plugin is at an early stage. If you encounter bugs, compatibility issues or totally missing functionality that must be in this plugin, I would appreciate if you [created an issue](https://github.com/felixarntz/wp-media-picker/issues). Or even better, if you can, do it yourself and [open a pull-request](https://github.com/felixarntz/wp-media-picker/pulls).
---
+++ wp-media-picker.js
@@ -0,0 +1,309 @@
+/*
+ * WP Media Picker -  version 0.1.0
+ *
+ * Felix Arntz <felix-arntz@leaves-and-love.net>
+ */
+
+( function( $, wp ) {
+
+	if ( typeof $.fn.wpMediaPicker !== 'undefined' ) {
+		// if the jQuery plugin is already defined, abort
+		return;
+	}
+
+	if ( typeof wp === 'undefined' || typeof wp.media === 'undefined' ) {
+		// if wp.media is not loaded, scaffold the jQuery plugin function and abort
+		$.fn.wpMediaPicker = function() {
+			return this;
+		};
+
+		return;
+	}
+
+	/**
+	 * Stores the original WordPress Core function for sending an attachment to the editor
+	 *
+	 * @type function
+	 */
+	var _orig_send_attachment = wp.media.editor.send.attachment;
+
+	/**
+	 * Stores the original global send_to_editor function which might be defined by a plugin
+	 *
+	 * @type function|undefined
+	 */
+	var _orig_send_to_editor = window.send_to_editor;
+
+	/**
+	 * Overrides the default send_to_editor function.
+	 *
+	 * Since WPMediaPicker uses this function to send HTML content to the preview div, we need to adjust this.
+	 *
+	 * @param string html the output to send
+	 */
+	var sendToEditor = function( html ) {
+		var wpActiveEditor = window.wpActiveEditor;
+
+		document.getElementById( wpActiveEditor ).innerHTML = html;
+
+		// always reset it back to the original after being executed
+		window.send_to_editor = _orig_send_to_editor;
+	};
+
+	/**
+	 * Returns the preview HTML output for an attachment.
+	 *
+	 * @param object attachment the attachment object to generate output for
+	 * @return string HTML output to send to the preview div
+	 */
+	var getMediaContent = function( attachment ) {
+		var output = '';
+
+		if ( 'image' === attachment.type ) {
+			// for image attachments, show the image
+			var src = attachment.url;
+			if ( attachment.sizes && attachment.sizes.large ) {
+				src = attachment.sizes.large.url;
+			} else if ( attachment.sizes && attachment.sizes.full ) {
+				src = attachment.sizes.full.url;
+			}
+			output += '<img src="' + src + '" alt="' + attachment.alt + '" />';
+		} else if ( 'video' === attachment.type ) {
+			// for video attachments, show the video player, optionally with the poster
+			var poster = '';
+			if ( attachment.image && attachment.image.src !== attachment.icon ) {
+				poster = attachment.image.src;
+			}
+			output += '<video class="wp-video-shortcode" preload="metadata"' + ( poster ? ' poster="' + poster + '"' : '' ) + ' controls><source type="' + attachment.mime + '" src="' + attachment.url + '" /></video>';
+		} else if ( 'audio' === attachment.type ) {
+			// for audio attachments, show the audio player, with either the cover or the mime type icon
+			if ( attachment.image && attachment.image.src && attachment.image.src !== attachment.icon ) {
+				output += '<img class="wp-audio-cover" src="' + attachment.image.src + '" alt="' + attachment.filename + '" />';
+			} else {
+				output += '<div class="mime-type-icon"><img src="' + attachment.icon + '" /><span>' + attachment.filename + '</span></div>';
+			}
+			output += '<audio class="wp-audio-shortcode" width="100%" preload="none" controls><source type="' + attachment.mime + '" src="' + attachment.url + '" /></audio>';
+		} else {
+			// if neither of the above formats, just show a mime type icon with the filename
+			output += '<div class="mime-type-icon"><img src="' + attachment.icon + '" /><span>' + attachment.filename + '</span></div>';
+		}
+
+		return output;
+	};
+
+	/**
+	 * Generates the basic plugin markup.
+	 *
+	 * This function is called when initializing the plugin on an element.
+	 *
+	 * @param jQuery $elem the input field to generate the markup for
+	 * @param object settings the settings for the input field
+	 * @param string media_content the preview HTML output for the field's attachment (optional)
+	 */
+	var generateMarkup = function( $elem, settings, media_content ) {
+		var input_id = $elem.attr( 'id' );
+		var button_text = media_content ? settings.label_replace : settings.label_add;
+		var remove_button_text = settings.label_remove;
+
+		if ( ! media_content ) {
+			media_content = '';
+		}
+
+		$elem
+			.hide()
+			.after(
+				'<a data-input-id="' + input_id + '" class="wp-mediapicker-button button" href="#">' + button_text + '</a>',
+				'<a data-input-id="' + input_id + '" class="wp-mediapicker-remove-button" href="#"' + ( media_content ? '' : ' style="display:none;"' ) + '>' + remove_button_text + '</a>',
+				'<div class="wp-mediapicker-content-wrap"><div id="wp-mediapicker-content-' + input_id + '" class="wp-mediapicker-content"' + ( media_content ? '' : ' style="display:none;"' ) + '>' + ( media_content ? media_content : '' ) + '</div></div>'
+			)
+			.next( '.wp-mediapicker-button' ).data( 'settings', settings );
+	};
+
+	/**
+	 * Overrides the WordPress Core function to send an attachment from the media modal to the input field.
+	 *
+	 * This function assigns the attachment ID or URL respectively to the input field.
+	 * Then it generates the preview content for the field and returns it.
+	 *
+	 * It is compatible with the original function: if the current modal is not a modal added by WPMediaPicker, the original function is executed instead.
+	 *
+	 * @param object props attachment properties
+	 * @param object attachment the attachment object to send to the field
+	 * @return string HTML content to send to the preview field
+	 */
+	wp.media.editor.send.attachment = function( props, attachment ) {
+		var wpActiveEditor;
+		if ( wp.media.editor.activeEditor ) {
+			wpActiveEditor = wp.media.editor.activeEditor;
+		} else {
+			wpActiveEditor = window.wpActiveEditor || '';
+		}
+		if ( 0 === wpActiveEditor.search( 'wp-mediapicker-content-' ) ) {
+			var content_id = wpActiveEditor;
+			var input_id = content_id.substring( 'wp-mediapicker-content-'.length );
+
+			var workflow = wp.media.editor.get( wpActiveEditor );
+			var settings = workflow.options.mediapicker_settings || {};
+
+			switch ( settings.store ) {
+				case 'ID':
+				case 'id':
+					$( '#' + input_id ).val( attachment.id );
+					break;
+				case 'url':
+					$( '#' + input_id ).val( attachment.url );
+					break;
+				default:
+					$( '#' + input_id ).val( attachment.url );
+			}
+
+			$( '.wp-mediapicker-button[data-input-id="' + input_id + '"]' ).text( settings.label_replace );
+			$( '.wp-mediapicker-remove-button[data-input-id="' + input_id + '"]' ).show();
+			$( '#' + content_id ).show();
+
+			window.send_to_editor = sendToEditor;
+
+			return getMediaContent( attachment );
+		} else {
+			return _orig_send_attachment.apply( this, [ props, attachment ] );
+		}
+	};
+
+	/**
+	 * This anonymous function opens the media modal for a field (and creates it if it does not exist already).
+	 *
+	 * It is executed whenever the Add/Replace button is clicked.
+	 *
+	 * Since the usual WordPress media modal contains a lot of functionality that would be useless in case of WPMediaPicker,
+	 * a custom type of modal is being used.
+	 */
+	$( document ).on( 'click', '.wp-mediapicker-button', function( e ) {
+		var $this = $( this );
+		var content_id = 'wp-mediapicker-content-' + $this.data( 'input-id' );
+		var settings = $( this ).data( 'settings' );
+		var workflow = wp.media.editor.get( content_id );
+
+		e.preventDefault();
+
+		if ( ! workflow ) {
+			workflow = wp.media.editor.add( content_id, {
+				frame: 'select',
+				state: 'insert',
+				states: [
+					new wp.media.controller.Library({
+						id:         'insert',
+						title:      settings.label_modal,
+						priority:   20,
+						toolbar:    'select',
+						filterable: settings.filterable,
+						library:    wp.media.query( settings.query ),
+						multiple:   false,
+						editable:   settings.editable,
+						allowLocalEdits: settings.allowLocalEdits,
+						displaySettings: settings.displaySettings,
+						displayUserSettings: settings.displayUserSettings
+					})
+				],
+				button: {
+					event: 'insert',
+					text: settings.label_button
+				},
+				mediapicker_settings: settings
+			});
+		}
+
+		wp.media.editor.open( content_id );
+	});
+
+	/**
+	 * This anonymous function removes the attachment for a field.
+	 *
+	 * It is executed whenever the Remove button is clicked.
+	 */
+	$( document ).on( 'click', '.wp-mediapicker-remove-button', function( e ) {
+		var $this = $( this );
+		var input_id = $this.data( 'input-id' );
+		var settings = $( '.wp-mediapicker-button[data-input-id="' + input_id + '"]' ).data( 'settings' );
+
+		e.preventDefault();
+
+		$( '#' + input_id ).val( null );
+
+		$( '.wp-mediapicker-button[data-input-id="' + input_id + '"]' ).text( settings.label_add );
+		$( '#wp-mediapicker-content-' + input_id ).hide().empty();
+
+		$this.hide();
+	});
+
+	/**
+	 * Initializes the plugin on one or more fields.
+	 *
+	 * This is the actual jQuery plugin function.
+	 *
+	 * In addition to providing settings in the function call, it is also possible to store field-related settings in a field directly.
+	 * It has to be valid JSON and it must be stored in a 'data-settings' attribute.
+	 *
+	 * @param object settings custom settings for the field (all optional)
+	 * @return jQuery
+	 */
+	$.fn.wpMediaPicker = function( settings ) {
+		settings = $.extend({
+			store: 'url',
+			query: {},
+			filterable: 'all',
+			searchable: true,
+			editable:   false,
+			allowLocalEdits: false,
+			displaySettings: false,
+			displayUserSettings: false,
+			label_add: wp.media.view.l10n.addMedia,
+			label_replace: wp.media.view.l10n.replace,
+			label_remove: wp.media.view.l10n.remove,
+			label_modal: wp.media.view.l10n.addMedia,
+			label_button: wp.media.view.l10n.addMedia
+		}, settings || {});
+
+		return this.each( function() {
+			var $elem = $( this );
+			var elem_settings = $.extend({}, settings );
+			var data_settings = $elem.data( 'settings' );
+
+			if ( data_settings ) {
+				if ( typeof data_settings === 'string' ) {
+					try {
+						data_settings = JSON.parse( data_settings );
+					} catch ( err ) {
+						console.error( err.message );
+					}
+				}
+				if ( typeof data_settings === 'object' ) {
+					elem_settings = $.extend( elem_settings, data_settings );
+				}
+			}
+
+			var attachment_id = parseInt( $elem.val(), 10 );
+
+			if ( attachment_id ) {
+				// if an attachment is set, make an AJAX call to get the attachment data and generate the preview output
+				wp.media.ajax({
+					type: 'POST',
+					data: {
+						action: 'get-attachment',
+						id: attachment_id
+					},
+					success: function( attachment ) {
+						generateMarkup( $elem, elem_settings, getMediaContent( attachment ) );
+					},
+					error: function() {
+						$elem.val( null );
+						generateMarkup( $elem, elem_settings );
+					}
+				});
+			} else {
+				// otherwise just generate the markup
+				$elem.val( null );
+				generateMarkup( $elem, elem_settings );
+			}
+		});
+	};
+}( jQuery, wp ) );
